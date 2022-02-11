2820816f9788f0fe59bd33e5a7876f0cc00faa35
‘zhouzi’ <zhouzi@gaoding.com>
2022-02-11 19:03:46 +0800

    1111

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,91 +1,104 @@
 <template>
-    <header class="header">
-        <div class="header-container">
-            <div class="header-left">
-                <div class="header__logo">
-                    <img src="../assets/logo.png" alt />
-                </div>
-                <el-menu
-                    class="el-menu-demo"
-                    mode="horizontal"
-                    @select="handleSelect"
-                    active-text-color="#fff"
-                    background-color="#1369bf"
-                    text-color="#fff"
-                >
-                    <el-menu-item index="1">首页</el-menu-item>
-                    <el-submenu index="2">
-                        <template slot="title">精华</template>
-                        <el-menu-item index="2-1">今日话题</el-menu-item>
-                        <el-menu-item index="2-2">名人访谈</el-menu-item>
-                        <el-menu-item index="2-3">找人</el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="3">
-                        <template slot="title">行情</template>
-                        <el-menu-item index="3-1">行情中心</el-menu-item>
-                        <el-menu-item index="3-2">筛选器</el-menu-item>
-                        <el-menu-item index="3-3">新股上市</el-menu-item>
-                        <el-menu-item index="3-3">买什么</el-menu-item>
-                    </el-submenu>
-                    <el-submenu index="4">
-                        <template slot="title">交易</template>
-                        <el-menu-item index="4-1">A股交易</el-menu-item>
-                        <el-menu-item index="4-2">蛋卷基金</el-menu-item>
-                        <el-menu-item index="4-3">私募中心</el-menu-item>
-                    </el-submenu>
-                </el-menu>
-
-                <div class="header__search">
-                    <el-select placeholder="搜索" size="small" multiple filterable remote></el-select>
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-sousuo" />
-                    </svg>
-                </div>
-            </div>
-            <div class="header-right">
-                <div class="header__app">下载app</div>
-                <button class="header__login">登录/注册</button>
-            </div>
+  <header class="header">
+    <div class="header-container">
+      <div class="header-left">
+        <div class="header__logo">
+          <img src="../assets/logo.png" alt />
         </div>
-    </header>
+        <el-menu
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          active-text-color="#fff"
+          background-color="#1369bf"
+          text-color="#fff"
+        >
+          <el-menu-item index="1" @click="goHome">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">精华</template>
+            <el-menu-item index="2-1">今日话题</el-menu-item>
+            <el-menu-item index="2-2">名人访谈</el-menu-item>
+            <el-menu-item index="2-3">找人</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">行情</template>
+            <el-menu-item index="3-1">行情中心</el-menu-item>
+            <el-menu-item index="3-2">筛选器</el-menu-item>
+            <el-menu-item index="3-3">新股上市</el-menu-item>
+            <el-menu-item index="3-3">买什么</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">交易</template>
+            <el-menu-item index="4-1">A股交易</el-menu-item>
+            <el-menu-item index="4-2">蛋卷基金</el-menu-item>
+            <el-menu-item index="4-3">私募中心</el-menu-item>
+          </el-submenu>
+        </el-menu>
+
+        <div class="header__search">
+          <el-select
+            placeholder="搜索"
+            v-model="value"
+            size="small"
+            multiple
+            filterable
+            remote
+          ></el-select>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-sousuo" />
+          </svg>
+        </div>
+      </div>
+      <div class="header-right">
+        <div class="header__app">下载app</div>
+        <button class="header__login">登录/注册</button>
+      </div>
+    </div>
+  </header>
 </template>
 <script>
 export default {
-    data() {
-        return {};
+  data() {
+    return {
+      activeIndex: "1",
+      value: [],
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     },
-    methods: {
-        handleSelect(key, keyPath) {
-            console.log(key, keyPath);
-        },
+    goHome() {
+      this.$router.push("/");
     },
+  },
 };
 </script>
 <style>
 .el-menu {
-    background-color: #fff !important;
+  background-color: #fff !important;
 }
 .header .el-menu-demo {
-    border: none !important;
+  border: none !important;
 }
 .header .el-menu-item {
-    border: 0px !important;
-    border: none !important;
+  border: 0px !important;
+  border: none !important;
 }
 .header .el-menu-demo > .el-menu-item {
-    font-size: 16px;
+  font-size: 16px;
 }
 .header .el-menu-demo .el-submenu__title {
-    font-size: 16px;
-    padding-left: 10px;
-    padding-right: 10px;
+  font-size: 16px;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 .header .el-submenu__title {
-    border: none !important;
+  border: none !important;
 }
 .el-menu--popup > .el-menu-item {
-    background: #fff !important;
-    color: #666c72 !important;
+  background: #fff !important;
+  color: #666c72 !important;
 }
 </style>
 <style>
@@ -94,62 +107,62 @@ export default {
 <style lang="less" scoped>
 @b: header;
 .@{b} {
-    position: fixed;
-    background: #1369bf;
-    left: 0;
-    top: 0;
-    z-index: 999;
-    width: 100%;
+  position: fixed;
+  background: #1369bf;
+  left: 0;
+  top: 0;
+  z-index: 999;
+  width: 100%;
+  color: #fff;
+  &-container {
+    width: 980px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+  &-left {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__search {
+    .icon {
+      font-size: 25px;
+      position: relative;
+      left: -35px;
+      top: 6px;
+    }
+  }
+  &__logo {
+    img {
+      width: 65px;
+      height: 25px;
+      vertical-align: middle;
+      margin-right: 10px;
+    }
+  }
+  &-right {
+    display: flex;
+  }
+  &__app {
+    font-size: 14px;
+    margin: auto;
+    margin-right: 10px;
+  }
+  &__login {
+    width: 128px;
+    padding: 6px 0px;
+    text-align: center;
+    font-size: 15px;
+    border: 1px #fff solid;
+    border-radius: 32px;
+    vertical-align: middle;
     color: #fff;
-    &-container {
-        width: 980px;
-        margin: auto;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        flex-direction: row;
-    }
-    &-left {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-    }
-    &__search{
-        .icon{
-            font-size: 25px;
-            position: relative;
-            left: -35px;
-            top: 6px;
-        }
-    }
-    &__logo {
-        img {
-            width: 65px;
-            height: 25px;
-            vertical-align: middle;
-            margin-right: 10px;
-        }
-    }
-    &-right {
-        display: flex;
-    }
-    &__app {
-        font-size: 14px;
-        margin: auto;
-        margin-right: 10px;
-    }
-    &__login {
-        width: 128px;
-        padding: 6px 0px;
-        text-align: center;
-        font-size: 15px;
-        border: 1px #fff solid;
-        border-radius: 32px;
-        vertical-align: middle;
-        color: #fff;
-        cursor: pointer;
-        background-color: transparent;
-    }
+    cursor: pointer;
+    background-color: transparent;
+  }
 }
 </style>
--- src/views/stock/index.vue
+++ src/views/stock/index.vue
@@ -1,189 +1,347 @@
 <template>
-    <div class="stock">
-        <div class="stock__links"></div>
-        <div class="stock__main">
-            <div class="stock__main-header">
-                <div class="stock__main__name">{{stockName[2]}}(SZ:{{stockName[1]}})</div>
-                <div class="stock__main__operate">
-                    <a class="stock__main__operate__add">+自选</a>
-                </div>
-            </div>
-            <div class="stock__main__price">
-                <strong>￥{{stockPrice[5]}}</strong>
-                <p>{{stockPrice[7]}}</p>
-                <p>{{stockPrice[8]}}%</p>
-            </div>
-            <div class="stock__main__info">
-                <span>最高:&nbsp;{{stockPrice[3]}}</span>
-                <span>今开:&nbsp;{{stockPrice[2]}}</span>
-                <span>涨停:&nbsp;{{stockPrice[3]}}</span>
-                <span>成交量:&nbsp;{{stockPrice[9]}}万手</span>
-
-                <span>最低:&nbsp;{{stockPrice[4]}}</span>
-                <span>昨收:&nbsp;{{stockPrice[5]}}</span>
-                <span>跌停:&nbsp;{{stockPrice[3]}}</span>
-                <span>成交额:&nbsp;{{stockPrice[10]}}亿</span>
-
-                <span>量比:&nbsp;{{stockInfo[5]}}</span>
-                <span>换手:&nbsp;{{stockInfo[3]}}%</span>
-                <span>市盈率:&nbsp;{{stockInfo[6]}}</span>
-                <span>市盈率(TTM):&nbsp;{{stockInfo[7]}}</span>
-
-                <span>市销率:&nbsp;{{stockInfo[8]}}</span>
-                <span>股息率:&nbsp;{{stockInfo[9]}}</span>
-                <span>股息率(TTM):&nbsp;{{stockInfo[10]}}</span>
-                <span>总股本:&nbsp;{{stockInfo[11]}}亿</span>
-
-                <span>总市值:&nbsp;{{stockInfo[16]}}亿</span>
-                <span>流通市值:&nbsp;{{stockInfo[17]}}亿</span>
-                <span>市场类型:&nbsp;{{stockName[5]}}</span>
-                <span>所属行业:&nbsp;{{stockName[4]}}</span>
-            </div>
-        </div>
-        <div class="stock__side"></div>
+  <div class="stock">
+    <div class="stock__links">
+      <div class="stock__links-item">
+        <div class="stock__links__title">公司概况</div>
+        <a class="stock__links__desc">公司简介</a>
+        <a class="stock__links__desc">公司高管</a>
+        <a class="stock__links__desc">IPO文件</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">股权信息</div>
+        <a class="stock__links__desc">股本结构</a>
+        <a class="stock__links__desc">十大股东</a>
+        <a class="stock__links__desc">十大流通股东</a>
+        <a class="stock__links__desc">基金持股</a>
+        <a class="stock__links__desc">高管增减持</a>
+        <a class="stock__links__desc">股东人数</a>
+        <a class="stock__links__desc">限售解禁</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">股票交易</div>
+        <a class="stock__links__desc">成交明细</a>
+        <a class="stock__links__desc">分价表</a>
+        <a class="stock__links__desc">大单统计</a>
+        <a class="stock__links__desc">大宗交易</a>
+        <a class="stock__links__desc">龙虎榜数据</a>
+        <a class="stock__links__desc">融资融券</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">公司公告</div>
+        <a class="stock__links__desc">最新公告</a>
+        <a class="stock__links__desc">年度报告</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">公司运作</div>
+        <a class="stock__links__desc">股东大会</a>
+        <a class="stock__links__desc">收入构成</a>
+        <a class="stock__links__desc">重大事项</a>
+        <a class="stock__links__desc">分红配送</a>
+        <a class="stock__links__desc">增发一览</a>
+        <a class="stock__links__desc">内部交易</a>
+        <a class="stock__links__desc">IR活动记录</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">财务数据</div>
+        <a class="stock__links__desc">业绩预告</a>
+        <a class="stock__links__desc">主要指标</a>
+        <a class="stock__links__desc">利润表</a>
+        <a class="stock__links__desc">资产负债表</a>
+        <a class="stock__links__desc">现金流量表</a>
+        <a class="stock__links__desc">所有者权益变动</a>
+        <a class="stock__links__desc">选股器</a>
+      </div>
+      <div class="stock__links-item">
+        <div class="stock__links__title">研究分析</div>
+        <a class="stock__links__desc">杜邦分析</a>
+        <a class="stock__links__desc">研究报告</a>
+      </div>
     </div>
+
+    <div class="stock__main">
+      <div class="stock__main-header">
+        <div class="stock__main__name">
+          {{ stockName[2] }}(SZ:{{ stockName[1] }})
+        </div>
+        <div class="stock__main__operate">
+          <a class="stock__main__operate__add">+自选</a>
+        </div>
+      </div>
+      <div class="stock__main__price" :class="{ redStock: stockUp }">
+        <strong>￥{{ stockPrice[5] }}</strong>
+        <p>{{ stockPrice[7] }}</p>
+        <p>{{ stockPrice[8] }}%</p>
+      </div>
+      <div class="stock__main__info">
+        <span :class="{ redStock: stockUp }"
+          >最高:&nbsp;{{ stockPrice[3] }}</span
+        >
+        <span>今开:&nbsp;{{ stockPrice[2] }}</span>
+        <span>涨停:&nbsp;{{ stockPrice[3] }}</span>
+        <span>成交量:&nbsp;{{ stockPrice[9] }}万手</span>
+
+        <span>最低:&nbsp;{{ stockPrice[4] }}</span>
+        <span>昨收:&nbsp;{{ stockPrice[5] }}</span>
+        <span>跌停:&nbsp;{{ stockPrice[3] }}</span>
+        <span>成交额:&nbsp;{{ stockPrice[10] }}亿</span>
+
+        <span>量比:&nbsp;{{ stockInfo[5] }}</span>
+        <span>换手:&nbsp;{{ stockInfo[3] }}%</span>
+        <span>市盈率:&nbsp;{{ stockInfo[6] }}</span>
+        <span>市盈率(TTM):&nbsp;{{ stockInfo[7] }}</span>
+
+        <span>市销率:&nbsp;{{ stockInfo[8] }}</span>
+        <span>股息率:&nbsp;{{ stockInfo[9] }}</span>
+        <span>股息率(TTM):&nbsp;{{ stockInfo[10] }}</span>
+        <span>总股本:&nbsp;{{ stockInfo[11] }}亿</span>
+
+        <span>总市值:&nbsp;{{ stockInfo[16] }}亿</span>
+        <span>流通市值:&nbsp;{{ stockInfo[17] }}亿</span>
+        <span>市场类型:&nbsp;{{ stockName[5] }}</span>
+        <span>所属行业:&nbsp;{{ stockName[4] }}</span>
+      </div>
+    </div>
+    <div class="stock__side">
+      <div class="stock__side-item">
+        <div class="stock__side__title">简介</div>
+        <div class="stock__side__desc">
+          <NEllipsis
+            :lines="2"
+            :show-more="log.showMore"
+            :text-style="{
+              'font-size': '12px',
+              color: '#000000',
+              'line-height': '22px',
+            }"
+            :text="log.cleanContent"
+            :origin-text="log.content"
+          >
+            <template #extra="extra">
+              <div
+                class="task-detail-extra"
+                :style="{
+                  opacity: extra?.isMore ? 1 : 0,
+                }"
+                @click="log.showMore = !log.showMore"
+              >
+                {{ !log.showMore ? "展开" : "收起" }}
+              </div>
+            </template> </NEllipsis
+          >{{ stockCompany[0] }}
+        </div>
+      </div>
+      <div class="stock__side-item">
+        <div class="stock__side__title">公司官网</div>
+        <a class="stock__side__desc" :href="'https://' + stockCompany[1]">
+          {{ stockCompany[1] }}
+        </a>
+      </div>
+      <div class="stock__side-item">
+        <div class="stock__side__title">主要业务</div>
+        <div class="stock__side__desc">{{ stockCompany[3] }}</div>
+      </div>
+      <div class="stock__side-item">
+        <div class="stock__side__title">经营范围</div>
+        <div class="stock__side__desc">{{ stockCompany[4] }}</div>
+      </div>
+
+      <div class="stock__side-item">
+        <div class="stock__side__title">联系邮箱</div>
+        <div class="stock__side__desc">{{ stockCompany[2] }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { PostStock } from "../../request/api";
 export default {
-    data() {
-        return {
-            stockId: "",
-            stockName: [],
-            stockInfo: [],
-            stockPrice: [],
-            stockUp: true,
-            stockData: "20220209",
-        };
-    },
-    created() {
-        this.getInfo();
-    },
-    mounted() {},
-    methods: {
-        getInfo() {
-            this.stockId = this.$route.params.id;
-            //获取股票基础信息:名字
-            PostStock({
-                api_name: "stock_basic",
-                params: {
-                    ts_code: this.stockId,
-                    limit: 3,
-                },
-            }).then((res) => {
-                this.stockName = res.data.data.items[0];
-                console.log(this.stockName);
-            });
-            //获取股票日线信息,价格，涨跌幅
-            PostStock({
-                api_name: "daily",
-                params: {
-                    ts_code: this.stockId,
-                },
-            }).then((res) => {
-                console.log(res.data.data.items[0]);
-                this.stockPrice = res.data.data.items[0];
-                console.log(this.stockData);
-                this.stockPrice[8] = this.stockPrice[8].toFixed(2);
-            });
-            //每日指标,包括市盈率、市净率、股息率、总股本等
-            PostStock({
-                api_name: "daily_basic",
-                params: {
-                    ts_code: this.stockId,
-                    // trade_date:this.stockData
-                },
-            }).then((res) => {
-                // console.log(res.data.data.items);
-                // todo:把小数点变为两位
-                res.data.data.items[0].forEach((item, index, array) => {
-                    console.log(item instanceof Number);
-                    if (index>2) {
-                        this.stockInfo.push(item);
-                        console.log(111);
-                    } else {
-                        this.stockInfo.push(item);
-                    }
-                });
-                console.log(this.stockInfo);
-                console.log(typeof this.stockInfo[4]);
-            });
+  data() {
+    return {
+      stockId: "",
+      stockName: [],
+      stockInfo: [],
+      stockPrice: [],
+      stockCompany: [],
+      stockUp: true,
+      stockData: "20220209",
+    };
+  },
+  created() {
+    this.getInfo();
+  },
+  mounted() {},
+  methods: {
+    getInfo() {
+      this.stockId = this.$route.params.id;
+      //获取股票基础信息:名字
+      PostStock({
+        api_name: "stock_basic",
+        params: {
+          ts_code: this.stockId,
+          limit: 3,
         },
+      }).then((res) => {
+        this.stockName = res.data.data.items[0];
+      });
+      //获取股票日线信息,价格，涨跌幅
+      PostStock({
+        api_name: "daily",
+        params: {
+          ts_code: this.stockId,
+        },
+      }).then((res) => {
+        this.stockPrice = res.data.data.items[0];
+        this.stockPrice[8] = this.stockPrice[8].toFixed(2);
+      });
+      //每日指标,包括市盈率、市净率、股息率、总股本等
+      PostStock({
+        api_name: "daily_basic",
+        params: {
+          ts_code: this.stockId,
+        },
+      }).then((res) => {
+        this.stockInfo = res.data.data.items[0].map((item, index, array) => {
+          if (typeof item === "number") {
+            return item.toFixed(2);
+          }
+        });
+      });
+      //获得公司简介，业务介绍
+      PostStock({
+        api_name: "stock_company",
+        params: {
+          ts_code: this.stockId,
+        },
+        fields: "introduction,main_business,business_scope,website,email",
+      }).then((res) => {
+        this.stockCompany = res.data.data.items[0];
+        console.log(this.stockCompany);
+      });
     },
+  },
 };
 </script>
 
 <style lang="less">
 .stock {
-    width: 1150px;
-    margin: 60px auto 0 auto;
-    display: flex;
-    flex-direction: row;
-    padding-top: 30px;
-    height: 1000px;
-    &__links {
-        width: 150px;
-        margin-left: 40px;
-        background-color: aqua;
+  width: 1150px;
+  margin: 60px auto 0 auto;
+  display: flex;
+  flex-direction: row;
+  padding-top: 30px;
+  &__links {
+    width: 150px;
+    margin-left: 40px;
+    &-item {
+      display: flex;
+      flex-direction: column;
+      margin-top: 10px;
+      margin-bottom: 20px;
     }
-    &__main {
-        width: 640px;
-        &-header {
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-start;
-        }
-        &__name {
-            line-height: 28px;
-            font-size: 20px;
-            color: #33353c;
-            letter-spacing: 0;
-            margin-bottom: 5px;
-        }
-        &__operate {
-            margin-left: auto;
-            &__add {
-                background: #06c;
-                color: #fff;
-                display: inline-block;
-                width: 90px;
-                height: 30px;
-                line-height: 30px;
-                text-align: center;
-                border-radius: 2px;
-                font-size: 14px;
-            }
-        }
-        &__price {
-            color: #d20;
-            margin-bottom: 10px;
-            strong {
-                font-size: 38px;
-            }
-            p {
-                padding-top: 15px;
-                display: inline-block;
-                margin-left: 10px;
-                font-size: 20px;
-            }
-        }
-        &__info {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            span {
-                display: inline-block;
-                width: 160px;
-                height: 22px;
-                font-size: 13px;
-                line-height: 20px;
-            }
-        }
+    &__title {
+      width: 110px;
+      font-size: 15px;
+      font-weight: 500;
+      color: #33353c;
+      border-bottom: 1px solid #edf0f5;
+      padding-bottom: 8px;
+      margin-bottom: 8px;
     }
-    &__side {
-        width: 280px;
-        background-color: aquamarine;
+    &__desc {
+      text-decoration: none;
+      line-height: 20px;
+      font-size: 14px;
+      color: #33353c;
+      margin-bottom: 5px;
+      cursor: pointer;
     }
+  }
+  //中间那部分
+  &__main {
+    width: 640px;
+    redStock {
+      color: #d20;
+    }
+    greenStock {
+      color: green;
+    }
+    &-header {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+    }
+    &__name {
+      line-height: 28px;
+      font-size: 20px;
+      color: #33353c;
+      letter-spacing: 0;
+      margin-bottom: 5px;
+    }
+    &__operate {
+      margin-left: auto;
+      &__add {
+        background: #06c;
+        color: #fff;
+        display: inline-block;
+        width: 90px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        border-radius: 2px;
+        font-size: 14px;
+      }
+    }
+    &__price {
+      color: #d20;
+      margin-bottom: 10px;
+      strong {
+        font-size: 38px;
+      }
+      p {
+        padding-top: 15px;
+        display: inline-block;
+        margin-left: 10px;
+        font-size: 20px;
+      }
+    }
+    &__info {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      span {
+        display: inline-block;
+        width: 160px;
+        height: 22px;
+        font-size: 13px;
+        line-height: 20px;
+      }
+    }
+  }
+  &__side {
+    width: 280px;
+    margin-left: 35px;
+    &-item {
+      margin-bottom: 30px;
+    }
+    &__title {
+      width: 280px;
+      font-size: 16px;
+      color: #33353c;
+      float: left;
+      font-weight: 700;
+      margin-bottom: 10px;
+      overflow: hidden;
+      border-bottom: 1px solid #edf0f5;
+      padding-bottom: 9px;
+    }
+    &__desc {
+      font-size: 14px;
+      word-break: break-all;
+      color: #33353c;
+      line-height: 21px;
+      text-decoration: none;
+    }
+  }
 }
 </style>
